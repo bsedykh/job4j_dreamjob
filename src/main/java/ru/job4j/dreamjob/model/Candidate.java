afb7b6d571c576a1ad614ee888306a82be5e49b9
Boris Sedykh <sedykh.boris@yandex.ru>
2024-02-12 17:56:57 +0300

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.model;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 
 public class Candidate {
     public static final Map<String, String> COLUMN_MAPPING = Map.of(
@@ -79,5 +80,22 @@ public class Candidate {
 
     public void setFileId(int fileId) {
         this.fileId = fileId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
